80144328c604b6bed635addc9a91458bdb60fe02
luiscunha98 <up201709375@up.pt>
2021-11-23 10:30:20 +0000

    updated settings.

--- auth.js
+++ auth.js
@@ -5,19 +5,20 @@ const rulesButton = document.getElementById("rules-button");
 const rulesDialog = document.getElementById("rules-dialog");
 const rankingButton = document.getElementById("ranking-button");
 const rankingDialog = document.getElementById("ranking-dialog");
-const botConfig = document.getElementById("bot-config-button");
-const settingsButton = document.getElementById("settings-button");
+const settingsDialog = document.getElementById("settings-dialog");
+const startButton = document.getElementById("play-button");
 
 authDialog.addEventListener("click", (event) => {event.stopPropagation()});
 rulesDialog.addEventListener("click", (event) => {event.stopPropagation()});
 rankingDialog.addEventListener("click", (event) => {event.stopPropagation()});
+settingsDialog.addEventListener("click", (event) => {event.stopPropagation()});
 
-signInButton.addEventListener("click", 
+signInButton.addEventListener("click",
                                         () => {
                                             backDrop.classList.remove("hidden");
                                             authDialog.classList.remove("hidden");
                                 });
-                                
+
 rulesButton.addEventListener("click",
                                     () => {
                                         backDrop.classList.remove("hidden");
@@ -30,14 +31,15 @@ rankingButton.addEventListener("click",
                                         rankingDialog.classList.remove("hidden");
                             });
 
-settings.addEventListener("click",
-                                    () => {
-                                        backDrop.classList.remove("hidden");
-                                        rankingDialog.classList.remove("hidden");
-                            });
 
-backDrop.addEventListener("click", 
+backDrop.addEventListener("click",
                                     ()=> {
                                         backDrop.classList.add("hidden");
                                         Array.from(backDrop.children).forEach((elem) => elem.classList.add("hidden"));
+                            });
+
+startButton.addEventListener("click",
+                                    () => {
+                                        backDrop.classList.remove("hidden");
+                                        settingsDialog.classList.remove("hidden");
                             });
